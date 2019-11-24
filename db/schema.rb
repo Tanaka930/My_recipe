88a2df891f8ca200e7ba0bc008e32cde432e3994
19990824 <wataru824@icloud.com>
2019-11-24 20:07:34 +0900

    コメントテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_24_082743) do
+ActiveRecord::Schema.define(version: 2019_11_24_110018) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image", null: false
+    t.text "text", null: false
+    t.string "material"
+    t.string "seasoning"
+    t.text "my_text", null: false
+    t.string "time"
+    t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -25,6 +39,11 @@ ActiveRecord::Schema.define(version: 2019_11_24_082743) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
+    t.string "icon"
+    t.text "prof"
+    t.date "birthday"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
