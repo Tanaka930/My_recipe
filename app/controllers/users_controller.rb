cc33542c2fda334be6801488b7f165b4a39cbf23
19990824 <wataru824@icloud.com>
2019-11-25 20:56:14 +0900

    birthday機能追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
 
+  def edit
+
+  end
+
+  def update
+
+  end
+
   def show
     @user = User.find_by(id: params[:id])
+    @users = User.new
   end
   
 end
