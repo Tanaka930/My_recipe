e7f31d99d08ddaf4d01620ec1f2769ada91b1d7b
Yuma Tanaka <yumat9637@gmail.com>
2019-11-28 23:08:31 +0900

    recipes#edit, #destory

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,6 @@
 class RecipesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_recipe, only: [:show, :edit, :update, :destroy]
 
   def index
     @recipes = Recipe.all
@@ -18,26 +19,27 @@ class RecipesController < ApplicationController
     end
   end
 
+  def show
+  end
+
   def edit
 
   end
 
   def update
-
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
+    end
   end
 
   def destroy
-
-  end
-
-  def show
-    @recipe = Recipe.find(params[:id])
-  end
-
-  def update
+    @recipe.destroy
   end
 
   private
+
   def recipe_params
     params.require(:recipe).permit(
       :name, 
@@ -54,5 +56,9 @@ class RecipesController < ApplicationController
       :content3,
       :content4
     ).merge(user_id: current_user.id)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
