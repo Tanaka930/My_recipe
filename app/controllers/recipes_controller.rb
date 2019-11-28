b9bc3862ebdb5f564ccdc6821784da9f19f4be59
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-28 21:15:00 +0900

    edit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.all.order("created_at DESC")
   end
 
   def  new
