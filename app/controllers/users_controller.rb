404afed468be817a1361935a708a4e52e422182a
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-28 21:02:11 +0900

    edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
   def edit
     @user = User.find_by(id: params[:id])
     @users = User.new
@@ -11,16 +20,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @users = User.new
+    @recipe = Recipe.where(user_id: @user.id)
+    binding.pry
   end
   
-
-  def index
-    @users = User.all
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
-    respond_to do |format|
-      format.html
-      format.json
-    end
-  end
 end
