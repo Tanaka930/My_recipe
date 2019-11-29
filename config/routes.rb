dc00f78a0a24c83024d20e804f1639035d444f20
ryoryo12 <pirata.kijinka12jango@ezweb.ne.jp>
2019-11-29 16:43:57 +0900

    editusers

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     get "signup", :to => "users/registrations#new"
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
+    get 'edit', :to => 'users#edit'
+    patch 'update', :to => 'users#update'
   end
   root 'recipes#index'
   resources :recipes
